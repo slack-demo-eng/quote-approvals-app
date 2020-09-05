bd5afc2ea690686ff956f0db2e5afcba0ed20e5a
George Fairbairn <george.fair@icloud.com>
2020-09-04 17:28:11 -0700

    send message to new channel

--- blocks/messages.js
+++ blocks/messages.js
@@ -1,19 +1,21 @@
+const moment = require("moment");
+
 module.exports = {
-  channel_exists: (companyName, channelName) => [
+  channel_exists: (companyName, channelId) => [
     {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: `:open_mouth: Oops! It looks like a discount approval has *already been requested* for ${companyName}. You can check it out in this channel here :point_right: #quote-approvals-${channelName}`,
+        text: `:open_mouth: Oops! It looks like a discount approval has *already been requested* for ${companyName}. You can check it out in this channel here :point_right: <#${channelId}>`,
       },
     },
   ],
-  channel_created: (companyName, channelName) => [
+  channel_created: (companyName, channelId) => [
     {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: `:tada: Awesome! You just started a discount approval process* for ${companyName}. You can watch it play out here :point_right: #quote-approvals-${channelName}`,
+        text: `:tada: Awesome! You just *started a discount approval process* for ${companyName}. You can watch it play out here :point_right: <#${channelId}>`,
       },
     },
   ],
@@ -47,6 +49,201 @@ module.exports = {
             emoji: true,
           },
           action_id: "cancel_ephemeral",
+        },
+      ],
+    },
+  ],
+  initial_status: (user, companyName, justification, discount) => [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `<@${user}> is requesting approval for *${companyName}*`,
+      },
+      accessory: {
+        type: "image",
+        image_url: "https://quote-approvals.s3.amazonaws.com/salesforce_1.png",
+        alt_text: "salesforce",
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":+1: *Approvals Required:*",
+      },
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":l1: > :l2: > :sales_ops: > :legal:",
+      },
+      accessory: {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Details",
+          emoji: true,
+        },
+        action_id: "status_details",
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":handshake: *Proposed Structure*",
+      },
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      fields: [
+        {
+          type: "mrkdwn",
+          text: "*Close Date:*",
+        },
+        {
+          type: "plain_text",
+          text: `${moment().format("MM-DD-YYYY")}`,
+        },
+        {
+          type: "mrkdwn",
+          text: "*ACV (Churn):*",
+        },
+        {
+          type: "plain_text",
+          text: "$0",
+        },
+        {
+          type: "mrkdwn",
+          text: "*Billings:*",
+        },
+        {
+          type: "plain_text",
+          text: "$0",
+        },
+        {
+          type: "mrkdwn",
+          text: "*Total Contract Value (TCV):*",
+        },
+        {
+          type: "plain_text",
+          text: "$45,974",
+        },
+        {
+          type: "mrkdwn",
+          text: "*Subscription Term:*",
+        },
+        {
+          type: "plain_text",
+          text: "12",
+        },
+      ],
+    },
+    {
+      type: "section",
+      fields: [
+        {
+          type: "mrkdwn",
+          text: "*Max Discount:*",
+        },
+        {
+          type: "mrkdwn",
+          text: `${discount}%`,
+        },
+        {
+          type: "mrkdwn",
+          text: "*Payment Terms:*",
+        },
+        {
+          type: "plain_text",
+          text: "Net 30",
+        },
+        {
+          type: "mrkdwn",
+          text: "*Payment Frequency:*",
+        },
+        {
+          type: "plain_text",
+          text: "Standard",
+        },
+      ],
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: " ",
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":ok_hand: *Asks & Justification:*",
+      },
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `${justification}`,
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: " ",
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":writing_hand: *Quote Lines:*",
+      },
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*Plus plan* x *250* seats with discount \`${discount}%\``,
+      },
+      accessory: {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Details",
+          emoji: true,
+        },
+        action_id: "quote_lines_details",
+      },
+    },
+    {
+      type: "actions",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Deal Stats",
+            emoji: true,
+          },
+          action_id: "deal_stats",
         },
       ],
     },
--- blocks/modals.js
+++ blocks/modals.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 module.exports = {
   launch_modal: {
     type: "modal",
@@ -90,6 +92,299 @@ module.exports = {
             type: "plain_text",
             text: "e.g. 10",
             emoji: true,
+          },
+        ],
+      },
+    ],
+  },
+  approval_details: {
+    type: "modal",
+    title: {
+      type: "plain_text",
+      text: "Approval Details",
+      emoji: true,
+    },
+    close: {
+      type: "plain_text",
+      text: "Close",
+    },
+    blocks: [
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*L1 Details*",
+          },
+          {
+            type: "plain_text",
+            text: "Non Standard Legal Language",
+          },
+          {
+            type: "mrkdwn",
+            text: "*L2 Details*",
+          },
+          {
+            type: "plain_text",
+            text: "Non Standard Legal Language",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*SalesOps Details*",
+          },
+          {
+            type: "plain_text",
+            text: "Non Standard Legal Language",
+          },
+          {
+            type: "mrkdwn",
+            text: "*Legal Details*",
+          },
+          {
+            type: "plain_text",
+            text: "Non Standard Legal Language",
+          },
+        ],
+      },
+    ],
+  },
+  quote_lines_details: {
+    type: "modal",
+    title: {
+      type: "plain_text",
+      text: "Quote Line Details",
+      emoji: true,
+    },
+    close: {
+      type: "plain_text",
+      text: "Close",
+    },
+    blocks: [
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Product Name:*",
+          },
+          {
+            type: "plain_text",
+            text: "Plus plan",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Quantity:*",
+          },
+          {
+            type: "plain_text",
+            text: "250",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Start Date:*",
+          },
+          {
+            type: "mrkdwn",
+            text: `${moment().format("MM-DD-YYYY")}`,
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*End Date:*",
+          },
+          {
+            type: "mrkdwn",
+            text: `${moment().add(1, "y").format("MM-DD-YYYY")}`,
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*One Time Credit:*",
+          },
+          {
+            type: "plain_text",
+            text: "$0",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*AOV:*",
+          },
+          {
+            type: "plain_text",
+            text: "$45,974",
+          },
+        ],
+      },
+    ],
+  },
+  deal_stats: {
+    type: "modal",
+    title: {
+      type: "plain_text",
+      text: "Deal Stats",
+      emoji: true,
+    },
+    close: {
+      type: "plain_text",
+      text: "Close",
+    },
+    blocks: [
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Employee Count:*",
+          },
+          {
+            type: "plain_text",
+            text: "2,450",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Active Seats Under Contract:*",
+          },
+          {
+            type: "plain_text",
+            text: "N/A",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Quote:*",
+          },
+          {
+            type: "mrkdwn",
+            text:
+              "<https://media.giphy.com/media/l9XgkOGzT3mm1TQCxW/giphy.gif|Q-12695>",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*New AOV:*",
+          },
+          {
+            type: "mrkdwn",
+            text: "$45,974",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Existing AOV:*",
+          },
+          {
+            type: "mrkdwn",
+            text: "$0",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Assigned EM:*",
+          },
+          {
+            type: "plain_text",
+            text: "N/A",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Type:*",
+          },
+          {
+            type: "plain_text",
+            text: "New Business",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Prior Year Opportunity AOV:*",
+          },
+          {
+            type: "plain_text",
+            text: "$0",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Uncapped Renewal Base:*",
+          },
+          {
+            type: "plain_text",
+            text: "$0",
+          },
+        ],
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: "*Has Invoice Teams:*",
+          },
+          {
+            type: "plain_text",
+            text: "Yes",
           },
         ],
       },
