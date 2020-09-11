4c90799f00db7440b48011b15b8e973dd5527f06
George Fairbairn <george.fair@icloud.com>
2020-09-10 17:11:36 -0700

    setup authorization flow

--- app.js
+++ app.js
@@ -30,8 +30,18 @@ dotenv.config();
 
 // intialize app
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: "my-state-secret",
+  scopes: [
+    "channels:history",
+    "channels:manage",
+    "chat:write",
+    "chat:write.public",
+    "commands",
+    "users:read",
+  ],
 });
 
 /* UTILITY FUNCTIONS 🧰 */
